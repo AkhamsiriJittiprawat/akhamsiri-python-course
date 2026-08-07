5aa3d4b2be019af2aedb9de1199325a236a237bc
AkhamsiriJittiprawat <akhamsiri.j@ku.th>
2026-08-07 12:08:45 +0700

    week 6

--- assignments/week06/quiz.py
+++ assignments/week06/quiz.py
@@ -26,11 +26,25 @@ def calculate_circle(radius):
 return string ที่จัดรูปแบบข้อมูลผู้ใช้
 รูปแบบ: "[username] (age: [age]) - [Premium User / Standard User]"
 
+อย่าลืมเขียนตัวแปรในส่วนของการทดลองใช้งานด้วย เอาซัก 3 ลูกค้า
+print(create_user_profile("Boonchoo", 40))
+print(create_user_profile("Manee",))
+print(create_user_profile("Pitti", 23, True))
+
 """
 
 def create_user_profile(username, age=18, premium=False):
     # Your Problem 3 solution
-    pass
+    if premium:
+        user_type = "Premium User"
+    else:
+        user_type = "Standard User"
+
+    return f"{username} (age: {age}) - {user_type}"
+
+print(create_user_profile("Boonchoo", 40))
+print(create_user_profile("Manee"))
+print(create_user_profile("Patti", 23, True))
 
 """ เขียน function ชื่อ analyze_scores ที่มีคุณสมบัติดังนี้:
 
