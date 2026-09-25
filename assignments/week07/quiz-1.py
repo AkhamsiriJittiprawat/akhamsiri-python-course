56651f4e6a3a2defb5b1ccd4fcc7d91886c5c923
AkhamsiriJittiprawat <akhamsiri.j@ku.th>
2026-09-25 11:59:46 +0700

    OOP1

--- assignments/week07/quiz-1.py
+++ assignments/week07/quiz-1.py
@@ -6,19 +6,44 @@
 """
 
 class Rectangle:
+    
     def __init__(self, length, width):
         self.length = length
         self.width = width
 
-    # Method to get the area
+    # Method to get the area 
     def get_area(self):
-        pass
+        return self.length * self.width
 
-    # Method to get the perimeter
+    # Method to get the perimeter 
     def get_perimeter(self):
-        pass
+        return f"Perimeter = {self.length} * {self.width} = {2 * (self.length + self.width)}"
 
 
 rect = Rectangle(10, 5)
 print(rect.get_area())       # Should print 50
 print(rect.get_perimeter())  # Should print 30
+
+
+
+"""
+ขอให้เขียนคลาส circlce ที่ทำงานคล้ายกับคลาส Rectangle 
+"""
+
+class Circle:
+
+    # ปรับให้สอดคล้องกับความเป็นวงกลม
+    def __init__(self, radius):
+        self.radius = radius
+
+    # Method to get the area ปรับสูตรพื้นที่วงกลม
+    def get_area(self):
+        return 3.14 * self.radius * self.radius
+
+    # Method to get the perimeter ปรับสูตรเส้นรอบรูปวงกลม
+    def get_perimeter(self):
+        return 2 * 3.14 * self.radius
+
+myCircle = Circle(10)
+print(myCircle.get_area())       
+print(myCircle.get_perimeter())
--- labs/week07-lab/example/class-draft.py
+++ labs/week07-lab/example/class-draft.py
@@ -1,15 +1,35 @@
+"""
+2 types of programming
+1) structured programming ==> การเขียนโปรแกรมเชิงดครงสร้าง > c , js , python
+2) object-oriented programming (OOP) ==> การเขียนโปรแกรมเชิงวัตถุ > java , c# , python
+"""
+
+# วิธีแก้ปัญหา : เป็นแค่แนวทาง template แม่แบบ เป็นเหมือนตรายาง
 class ClassName:
     """Class docstring"""
-    
+
+    # ข้อมูลที่ต้องใช้ในการแก้ปัญหา ระบุไว้ใน constructor method
     def __init__(self, parameters):
         # Constructor method
         self.attribute = value
-    
+
+    # การกระทำ ==> method
     def method_name(self):
         # Instance method
         return something
 
+    def method_name2(self):
+        pass
 
+# การสร้างวัตถุจากคลาส ==> เอาคลาสมาใช้
 myObj = ClassName(parameters)
+
+# ใช้งานวัตถุจากคลาส
 print(myObj.attribute)
 resultFromMethod = myObj.method_name()
+myObj.method_name2()
+
+myobj2 = ClassName(parameters)
+print(myObj.attribute)
+print(myObj.method_name())
+myObj.method_name2()
